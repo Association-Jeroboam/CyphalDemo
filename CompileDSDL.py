a08237c3b7849aff09f5a8c85e26cffba62b8cd1
Thomas Jeantet <tj.thomas.jeantet@gmail.com>
2022-05-06 22:22:29 +0200

    DSDL compiling script

---
+++ CompileDSDL.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import distutils.command.build_py
+from pathlib import Path
+
+OUTPATH = "./"
+SUBDIR = ""
+OUTDIR =".dsdl_compiled"
+
+
+# noinspection PyUnresolvedReferences
+
+import pycyphal
+
+pycyphal.dsdl.compile_all(
+    [
+        "public_regulated_data_types/uavcan",  # All Cyphal applications need the standard namespace, always.
+        "public_regulated_data_types/reg",  # Many applications also need the non-standard regulated DSDL.
+        "jeroboam",
+    ],
+    output_directory=Path(OUTPATH, SUBDIR, OUTDIR),  # Store in the build output archive.
+)
